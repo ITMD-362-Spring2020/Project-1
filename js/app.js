f3f8b3bf924550980e1696be449dd8eb4693a73e
Alif Ishmam <54598961+aishmam1999@users.noreply.github.com>
2020-02-26 19:41:26 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 
+
+    let subtotal = document.getElementById("subtotal");
+    let taxRate = document.getElementById("taxRate");
+    let  SalesTax = document.getElementById("salesTax");
+    let  total = document.getElementById("total");
+    const calculateBtn = document.getElementById("calculateBtn");
+    const clearBtn = document.getElementById("clearBtn");
+    
+    
+    console.log(parseFloat(subtotal.value) + parseFloat(taxRate.value));
+    
+    calculateBtn.addEventListener("click", () => {
+        if( (parseFloat(subtotal.value)  > 0)  &&  ( (parseFloat(taxRate.value)  > 0)  && (parseFloat(taxRate.value)  < 20) )) {
+        total.value = (parseFloat(subtotal.value) + (parseFloat((taxRate.value)/100)* parseFloat(subtotal.value)) + parseFloat(SalesTax.value)*parseFloat(subtotal.value));
+        }
+        else {
+            alert("Invalid input");
+        }
+    });
+    
+    clearBtn.addEventListener("click", () => {
+        subtotal.value = null;
+        taxRate.value = null;
+        total.value = null;
+    });
+    
